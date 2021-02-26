96883ddf5584e5b3928f69164b495462c0ab30e6
Shreyan Kumar Mishra <79162142+Shreyan34@users.noreply.github.com>
2021-02-26 15:31:31 +0530

    This is the prediction algorithm

---
+++ PredictSeats.py
@@ -0,0 +1,32 @@
+#importing models
+from matplotlib import pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+#the machine learning model
+prediction_year = int(input('Enter the year of prediction:'))
+round(prediction_year)
+data = pd.read_csv('Seats VS Years (CSV).csv')
+
+#the bar graph
+plt.title('The Model So Far')
+plt.plot(data['Years'], data['Seats'], color='red')
+plt.bar(data['Years'], data['Seats'], color='orange')
+plt.grid()
+plt.ylabel('No. of seats won')
+plt.xlabel('Election Years')
+plt.show()
+print(data)
+print('\n')
+
+model = LinearRegression()
+model.fit(data[['Years']], data[['Seats']])
+
+if prediction_year < 2021:
+    print('In the year of',prediction_year, 'BJP won (prediction):', model.predict([[prediction_year]]), 'seats.')
+elif prediction_year > 2021:
+    print('In the year of',prediction_year, 'BJP is going to win:', model.predict([[prediction_year]]), 'seats.')
+elif prediction_year == 2021:
+    print('In the year of',prediction_year, 'BJP is going to win:', model.predict([[prediction_year]]), 'seats.')
+else:
+    print(model.predict([[prediction_year]]))
